15eee1e76d04ae68f6fc27cd7eec42d93cad7b5d
Sario Tedesco <sariotedesco@gmail.com>
2023-03-05 00:01:28 +0100

    ver-0.1

---
+++ bot.py
@@ -0,0 +1,52 @@
+import discord
+import responses
+from keep_alive import keep_alive
+import os
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    # App ID
+    # os.environ['token']
+    TOKEN = 'MTA3OTc4MjQzODk3NDI3NTY5NQ.GBnhws.TfCaW9u-nDo4NxvvwLljj52ojEliz7qzaYSLvw'
+    # intents
+    intents = discord.Intents.default()
+    intents.message_content = True
+    # Client
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now Alive!')
+
+    @client.event
+    async def on_message(message):
+        # Make sure bot doesn't get stuck in an infinite loop
+        if message.author == client.user:
+            return
+
+        # Get data about the user
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        # Debug printing
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        # If the user message contains a '?' in front of the text, it becomes a private message
+        if user_message[0] == '?':
+            user_message = user_message[1:]  # [1:] Removes the '?'
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    # Program Start!
+    keep_alive()
+    client.run(TOKEN)
---
+++ keep_alive.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import logging
+from threading import Thread
+
+app = Flask('')
+# disables the connection infos
+log = logging.getLogger('werkzeug')
+log.disabled = True
+
+
+@app.route('/')
+def home():
+    return "Keep the BOT alive!"
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
