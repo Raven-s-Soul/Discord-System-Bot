981ebf518afccfc9a9d1ac4c6168a3434d3f4d30
Mark8948 <127618431+Mark8948@users.noreply.github.com>
2023-07-14 16:23:55 +0200

    Update responses by Marcolicchio.py

--- responses.py
+++ responses.py
@@ -9,33 +9,32 @@ def handle_response(message) -> str:
         return 'Yahallo!'
 
     if p_message == 'helo':
-        return 'helo a te <3!'
+        return 'heeeelo back!'
 
     if p_message == 'roll':
+        print('Number beetween 1 and 6 coming!!!')
         return str(random.randint(1, 6))
 
-    if p_message == 'help':
+    if p_message == '+help':
         return """
 ```
-General commands:
-music    - Show Music commands
-hello    - Answer back
-roll     - Random number from 1 to 6
+General commands (type help pagename to summon the right help menu):
+help music   ====>     Shows Music commands
+hello        ====>     Answers back
+roll         ====>     Generates a random number between 1 and 6
 """ + config.global_operator + """repeat  - times string/text (10 capped)
 ```
 """
-    if p_message == 'music':
+    if p_message == 'help music':
         return """
 ```
 Music commands:
-""" + config.global_operator + """play    - Play a song & join the channel
+""" + config.global_operator + """play    - Plays a song & joins the channel
 """ + config.global_operator + """pause   - Pauses the current song being played
 """ + config.global_operator + """resume  - Resumes playing the current song
-""" + config.global_operator + """stop    - Stop play & disconnect from the channel
-""" + config.global_operator + """join    - Join channel
-""" + config.global_operator + """leave   - Leave & Pause
+""" + config.global_operator + """stop    - Stops playing & disconnects from the channel
+""" + config.global_operator + """join    - Joins channel
+""" + config.global_operator + """leave   - Leaves & Pauses
 ```
 """
-
-
-#  return 'Yeah, I don\'t know. Try typing "!help".'
+        
